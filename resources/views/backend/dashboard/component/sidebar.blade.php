c5d09f84d1f1e1756ee10e2805dd766dfe2db636
Nguyễn Viết Dương <duongnv10504@gmail.com>
2024-07-24 10:10:00 +0700

    main

--- resources/views/backend/dashboard/component/sidebar.blade.php
+++ resources/views/backend/dashboard/component/sidebar.blade.php
@@ -10,8 +10,10 @@
                         <img alt="image" class="img-circle" src="backend/img/profile_small.jpg" />
                     </span>
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Admin</strong>
-                            </span> <span class="text-muted text-xs block"> <b class="caret">Admin</b></span>
+                        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Nguyễn Viết Dương
+                                    ADMIN</strong>
+                            </span> <span class="text-muted text-xs block"> <b class="caret">Nguyễn Viết Dương
+                                    ADMIN</b></span>
                         </span> </a>
                     <ul class="dropdown-menu animated fadeInRight m-t-xs">
                         <li><a href="profile.html">Profile</a></li>
@@ -28,8 +30,8 @@
             @foreach (config('apps.sidebar.module') as $key => $val)
                 <li class="{{ in_array($segment, $val['name']) ? 'active' : '' }}">
                     @if (isset($val['subModule']))
-                        <a href=""><i class="{{ $val['icon'] }}"></i> <span
-                                class="nav-label">{{ $val['title'] }}</span> <span class="fa arrow"></span></a>
+                        <a href=""><i class="{{ $val['icon'] }}"></i> <span class="nav-label">{{ $val['title'] }}</span> <span
+                                class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             @foreach ($val['subModule'] as $module)
                                 <li><a href="{{ $module['route'] }}">{{ $module['title'] }}</a></li>
